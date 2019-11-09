93ff5bfdcfffd85078ec4802739ca2c0c7af2bc1
Rooshan <itsrooshan@gmail.com>
2019-11-09 08:04:22 -0700

    Semantic-ui working partially

--- app/App.jsx
+++ app/App.jsx
@@ -1,9 +1,27 @@
 import React from "react";
+import {Button, Dropdown, Card, Image, Icon} from "semantic-ui-react";
 
 class App extends React.Component {
     render() {
         return (
-            <h2>Test</h2>
+            <Card>
+            <Image src='/images/avatar/large/matthew.png' wrapped ui={false} />
+            <Card.Content>
+              <Card.Header>Matthew</Card.Header>
+              <Card.Meta>
+                <span className='date'>Joined in 2015</span>
+              </Card.Meta>
+              <Card.Description>
+                Matthew is a musician living in Nashville.
+              </Card.Description>
+            </Card.Content>
+            <Card.Content extra>
+              <a>
+                <Icon name='user' />
+                22 Friends
+              </a>
+            </Card.Content>
+          </Card>
         );
     }
 }
