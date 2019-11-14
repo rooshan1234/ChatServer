9557e445afe9f9d7ce064808e110a524fa2be450
Rooshan <itsrooshan@gmail.com>
2019-11-13 21:16:01 -0700

    Added bother all option to send a message to everyone

--- src/app/chat/forms/SendMessageForm.jsx
+++ src/app/chat/forms/SendMessageForm.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Form } from "semantic-ui-react";
+import { Form, Checkbox } from "semantic-ui-react";
 import { connect } from "react-redux";
 import { withRouter } from "react-router-dom";
 
@@ -9,15 +9,19 @@ import styles from "./SendMessageForm.less";
 class SendMessageForm extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { text: "" };
+    this.state = { text: "", botherEveryone: false };
   }
 
   handleChange = (e, { name, value }) => {
     this.setState({ [name]: value });
   };
 
+  handleChangeOnCheckBox = (e, { name, checked }) => {
+    this.setState({ [name]: checked });
+  };
+
   handleSubmit = e => {
-    const { text } = this.state;
+    const { text, botherEveryone } = this.state;
 
     let currentDate = new Date();
     let formattedDate =
@@ -28,19 +32,32 @@ class SendMessageForm extends React.Component {
       currentDate.getDate();
 
     e.preventDefault();
-    this.props.addMessageToChat(
-      this.props.chatId,
-      text,
-      this.props.recipientUserId,
-      "recieved",
-      formattedDate
-    );
 
-    this.setState({ text: "" });
+    if (botherEveryone) {
+      this.props.chats.map(chat => {
+        this.props.addMessageToChat(
+          chat.id,
+          text,
+          this.props.recipientUserId,
+          "recieved",
+          formattedDate
+        );
+      });
+    } else {
+      this.props.addMessageToChat(
+        this.props.chatId,
+        text,
+        this.props.recipientUserId,
+        "recieved",
+        formattedDate
+      );
+    }
+
+    this.setState({ text: "", botherEveryone: false });
   };
 
   render() {
-    const { text } = this.state;
+    const { text, botherEveryone } = this.state;
 
     return (
       <Form onSubmit={this.handleSubmit}>
@@ -51,6 +68,10 @@ class SendMessageForm extends React.Component {
             placeholder="Message..."
             value={text}
             onChange={this.handleChange}
+            error={{
+              content: "You must agree to the terms and conditions",
+              pointing: "right"
+            }}
           />
           <Form.Button
             className={styles.sendButton}
@@ -58,13 +79,24 @@ class SendMessageForm extends React.Component {
             content="Send"
           />
         </Form.Group>
+        <Form.Group>
+          <Form.Field>
+            <Checkbox
+              name="botherEveryone"
+              label="Bother Everyone"
+              checked={botherEveryone}
+              onChange={this.handleChangeOnCheckBox}
+            />
+          </Form.Field>
+        </Form.Group>
       </Form>
     );
   }
 }
 
 const mapStateToProps = state => ({
-  recipientUserId: state.recipient_user_id
+  recipientUserId: state.recipient_user_id,
+  chats: state.chats
 });
 
 const mapDispatchToProps = dispatch => ({
