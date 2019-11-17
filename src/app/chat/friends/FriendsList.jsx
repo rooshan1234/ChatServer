7eacaf4bdc4d3a660b1df2dfa1a5735b414389a8
Rooshan <itsrooshan@gmail.com>
2019-11-16 19:29:41 -0700

    Turned friends list into functional component and added dist folder to .gitignore

--- src/app/chat/friends/FriendsList.jsx
+++ src/app/chat/friends/FriendsList.jsx
@@ -2,27 +2,21 @@ import React from "react";
 import { BrowserRouter as Router, NavLink, withRouter } from "react-router-dom";
 import { Menu, Icon, Label } from "semantic-ui-react";
 
-class FriendsList extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <Menu fluid secondary vertical stackable>
-        <Menu.Item as={NavLink} to={"/chat/1"} activeClassName="active">
-          <Label color="teal">1</Label>
-          Sonny
-        </Menu.Item>
-        <Menu.Item as={NavLink} to={"/chat/2"}>
-          Fredo
-        </Menu.Item>
-        <Menu.Item as={NavLink} to={"/chat/3"}>
-          Michael
-        </Menu.Item>
-      </Menu>
-    );
-  }
+function FriendsList() {
+  return (
+    <Menu fluid secondary vertical stackable>
+      <Menu.Item as={NavLink} to={"/chat/1"}>
+        <Label color="teal">1</Label>
+        Sonny
+      </Menu.Item>
+      <Menu.Item as={NavLink} to={"/chat/2"}>
+        Fredo
+      </Menu.Item>
+      <Menu.Item as={NavLink} to={"/chat/3"}>
+        Michael
+      </Menu.Item>
+    </Menu>
+  );
 }
 
 export default withRouter(FriendsList);
