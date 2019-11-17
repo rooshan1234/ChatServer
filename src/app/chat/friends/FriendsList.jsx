57b9c5eba6748635f32943703d1f9f5ce0364851
Rooshan <itsrooshan@gmail.com>
2019-11-16 19:23:51 -0700

    Fixed unnecessary local state under friends list

--- src/app/chat/friends/FriendsList.jsx
+++ src/app/chat/friends/FriendsList.jsx
@@ -5,49 +5,19 @@ import { Menu, Icon, Label } from "semantic-ui-react";
 class FriendsList extends React.Component {
   constructor(props) {
     super(props);
-
-    this.state = { activeItem: this.props.chatId || "1" };
   }
-
-  componentDidUpdate(prevProps, prevState, snapshot) {
-    if (this.props.chatId && prevState.activeItem !== this.props.chatId) {
-      this.setState({ activeItem: this.props.chatId });
-    }
-  }
-
-  handleItemClick = (e, { name }) => this.setState({ activeItem: name });
 
   render() {
-    const { activeItem } = this.state;
-
     return (
       <Menu fluid secondary vertical stackable>
-        <Menu.Item
-          as={NavLink}
-          to={"/chat/1"}
-          active={activeItem === "1"}
-          name="1"
-          onClick={this.handleItemClick}
-        >
+        <Menu.Item as={NavLink} to={"/chat/1"} activeClassName="active">
           <Label color="teal">1</Label>
           Sonny
         </Menu.Item>
-        <Menu.Item
-          as={NavLink}
-          to={"/chat/2"}
-          active={activeItem === "2"}
-          name="2"
-          onClick={this.handleItemClick}
-        >
+        <Menu.Item as={NavLink} to={"/chat/2"}>
           Fredo
         </Menu.Item>
-        <Menu.Item
-          as={NavLink}
-          to={"/chat/3"}
-          name="3"
-          active={activeItem === "3"}
-          onClick={this.handleItemClick}
-        >
+        <Menu.Item as={NavLink} to={"/chat/3"}>
           Michael
         </Menu.Item>
       </Menu>
